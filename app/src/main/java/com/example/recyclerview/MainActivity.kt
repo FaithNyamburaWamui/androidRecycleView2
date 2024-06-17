2ad3ededa51e51a5548892f55c728836d5753924
student <faithnyamburawamui@gmail.com>
2024-06-17 14:58:47 +0300

    first commit

---
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.recyclerview
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclerview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+       val names= listOf("Amina","Caroline","Brenda","Daina","Esther","Flo","Gina","Hellen","Innocent","Jacob","Kelvin","Lisa","Mike","Naomi","Oscar","Pearl","Queen","Robert")
+  binding.rvNames.layoutManager=LinearLayoutManager(this)
+        val nameAdapter=NamesRecyclerViewAdapter(names)
+        binding.rvNames.adapter=nameAdapter
+    }
+}
